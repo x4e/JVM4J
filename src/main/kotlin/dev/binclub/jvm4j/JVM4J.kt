095ca8b305a3b299f87b076136878366f1c4fb58
mastercooker <c.o.okiedragon234@gmail.com>
2020-09-26 19:55:15 +0100

    Compiler, throwable ahd thread implementations

--- src/main/kotlin/dev/binclub/jvm4j/JVM4J.kt
+++ src/main/kotlin/dev/binclub/jvm4j/JVM4J.kt
@@ -42,4 +42,40 @@ object JVM4J {
 	@JvmOverloads
 	external fun arrayCopy(ignored: Class<*>? = null, src: Any?, src_pos: Int, dst: Any?, dst_pos: Int, length: Int)
 	external fun initProperties(p: Any?): Any?
+	external fun onExit(op: Runnable) // TODO: Not yet implemented
+	external fun exit(code: Int)
+	external fun halt(code: Int)
+	external fun gc()
+	external fun maxObjectInspectionAge(): Long
+	external fun traceInstructions(on: Boolean)
+	external fun traceMethodCalls(on: Boolean)
+	external fun totalMemory(): Long
+	external fun freeMemory(): Long
+	external fun maxMemory(): Long
+	external fun activeProcessorCount(): Int
+	external fun loadLibrary(name: String): Long
+	external fun unloadLibrary(handle: Long)
+	external fun findLibraryEntry(handle: Long, name: String): Long
+	external fun isSupportedJNIVersion(version: Int): Boolean
+	external fun isNaN(d: Double): Boolean
+	external fun fillInStackTrace(throwable: Throwable)
+	external fun getStackTraceDepth(throwable: Throwable): Int
+	external fun getStackTraceElement(throwable: Throwable, index: Int): Any?
+	external fun initializeCompiler(compCls: Class<*>)
+	external fun isSilentCompiler(compCls: Class<*>): Boolean
+	external fun compileClass(compCls: Class<*>, cls: Class<*>): Boolean
+	external fun compileClasses(compCls: Class<*>, name: String): Boolean
+	external fun compilerCommand(compCls: Class<*>, arg: Any?): Any
+	external fun startThread(thread: Thread)
+	external fun stopThread(thread: Thread, exception: Any?)
+	external fun isThreadAlive(thread: Thread): Boolean
+	external fun suspendThread(thread: Thread)
+	external fun resumeThread(thread: Thread)
+	external fun setThreadPriority(thread: Thread, priority: Int)
+	external fun yield(threadClass: Class<*>?)
+	external fun sleep(threadClass: Class<*>?, ms: Long)
+	external fun currentThread(threadClass: Class<*>?): Thread?
+	external fun countStackFrames(thread: Thread): Int
+	external fun interrupt(thread: Thread)
+	external fun isInterrupted(thread: Thread, clearInterrupted: Boolean): Boolean
 }
--- src/main/rust/jvm.rs
+++ src/main/rust/jvm.rs
@@ -1,9 +1,9 @@
-#[macro_use]
-extern crate rs_jvm_bindings;
-
-use rs_jvm_bindings::jni::{JavaVM, JNI_VERSION_1_8, jlong, JNIEnv, jobject, JNI_OK, jint, jstring, jclass};
+use rs_jvm_bindings::jni::{JavaVM, JNI_VERSION_1_8, jlong, JNIEnv, jobject, JNI_OK, jint, jstring, jclass, jmethodID, jboolean, jdouble, jthrowable};
 use rs_jvm_bindings::utils::*;
 use rs_jvm_bindings::jvm::*;
+use std::os::raw::c_void;
+use std::borrow::BorrowMut;
+use rs_jvm_bindings::jvmti::jthread;
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_getInterfaceVersion(
@@ -90,4 +90,329 @@ pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_initProperties(
 	p: jobject
 ) -> jobject {
 	JVM_InitProperties(env, p)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_onExit(
+	env: *mut JNIEnv, _this: jobject,
+	func: jobject
+) {
+	panic!("Not yet implemented");
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_exit(
+	env: *mut JNIEnv, _this: jobject,
+	code: jint
+) {
+	JVM_Exit(code);
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_halt(
+	env: *mut JNIEnv, _this: jobject,
+	code: jint
+) {
+	JVM_Halt(code);
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_gc(
+	env: *mut JNIEnv, _this: jobject
+) {
+	JVM_GC()
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_maxObjectInspectionAge(
+	env: *mut JNIEnv, _this: jobject
+) -> jlong {
+	JVM_MaxObjectInspectionAge()
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_traceInstructions(
+	env: *mut JNIEnv, _this: jobject,
+	on: jboolean
+) {
+	JVM_TraceInstructions(on)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_traceMethodCalls(
+	env: *mut JNIEnv, _this: jobject,
+	on: jboolean
+) {
+	JVM_TraceMethodCalls(on)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_totalMemory(
+	env: *mut JNIEnv, _this: jobject
+) -> jlong {
+	JVM_TotalMemory()
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_freeMemory(
+	env: *mut JNIEnv, _this: jobject
+) -> jlong {
+	JVM_FreeMemory()
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_maxMemory(
+	env: *mut JNIEnv, _this: jobject
+) -> jlong {
+	JVM_MaxMemory()
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_activeProcessorCount(
+	env: *mut JNIEnv, _this: jobject
+) -> jint {
+	JVM_ActiveProcessorCount()
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_loadLibrary(
+	env: *mut JNIEnv, _this: jobject,
+	name: jstring
+) -> jlong {
+	let mut is_copy: jboolean = 0;
+	let cname = (**env).GetStringUTFChars.unwrap()(env, name, is_copy.borrow_mut());
+	let cout = JVM_LoadLibrary(cname);
+	(**env).ReleaseStringUTFChars.unwrap()(env, name, cname);
+	cout as jlong
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_unloadLibrary(
+	env: *mut JNIEnv, _this: jobject,
+	handle: jlong
+) {
+	JVM_UnloadLibrary(handle as *mut c_void)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_findLibraryEntry(
+	env: *mut JNIEnv, _this: jobject,
+	handle: jlong, name: jstring
+) -> jlong {
+	let mut is_copy: jboolean = 0;
+	let cname = (**env).GetStringUTFChars.unwrap()(env, name, is_copy.borrow_mut());
+	let cout = JVM_FindLibraryEntry(handle as *mut c_void, cname);
+	(**env).ReleaseStringUTFChars.unwrap()(env, name, cname);
+	cout as jlong
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_isSupportedJNIVersion(
+	env: *mut JNIEnv, _this: jobject,
+	version: jint
+) -> jboolean {
+	JVM_IsSupportedJNIVersion(version)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_isNaN(
+	env: *mut JNIEnv, _this: jobject,
+	d: jdouble
+) -> jboolean {
+	JVM_IsNaN(d)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_fillInStackTrace(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable
+) {
+	JVM_FillInStackTrace(env, throwable)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_getStackTraceDepth(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable
+) -> jint {
+	JVM_GetStackTraceDepth(env, throwable)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_getStackTraceElement(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable, index: jint
+) -> jobject {
+	JVM_GetStackTraceElement(env, throwable, index)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_initializeCompiler(
+	env: *mut JNIEnv, _this: jobject,
+	compCls: jclass
+) {
+	JVM_InitializeCompiler(env, compCls)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_isSilentCompiler(
+	env: *mut JNIEnv, _this: jobject,
+	compCls: jclass
+) -> jboolean {
+	JVM_IsSilentCompiler(env, compCls)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_compileClass(
+	env: *mut JNIEnv, _this: jobject,
+	compCls: jclass, cls: jclass
+) -> jboolean {
+	JVM_CompileClass(env, compCls, cls)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_compileClasses(
+	env: *mut JNIEnv, _this: jobject,
+	compCls: jclass, name: jstring
+) -> jboolean {
+	JVM_CompileClasses(env, compCls, name)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_compilerCommand(
+	env: *mut JNIEnv, _this: jobject,
+	compCls: jclass, arg: jobject
+) -> jobject {
+	JVM_CompilerCommand(env, compCls, arg)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_startThread(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jobject
+) {
+	JVM_StartThread(env, thread)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_stopThread(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jobject, exception: jobject
+) {
+	JVM_StopThread(env, thread, exception)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_isThreadAlive(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jobject
+) -> jboolean {
+	JVM_IsThreadAlive(env, thread)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_suspendThread(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jobject
+) {
+	JVM_SuspendThread(env, thread)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_resumeThread(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jthread
+) {
+	JVM_ResumeThread(env, thread)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_setThreadPriority(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jthread, priority: jint
+) {
+	JVM_SetThreadPriority(env, thread, priority)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_yield(
+	env: *mut JNIEnv, _this: jobject,
+	thread_class: jclass
+) {
+	JVM_Yield(env, thread_class)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_sleep(
+	env: *mut JNIEnv, _this: jobject,
+	thread_class: jclass, ms: jlong
+) {
+	JVM_Sleep(env, thread_class, ms)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_currentThread(
+	env: *mut JNIEnv, _this: jobject,
+	thread_class: jclass
+) -> jobject {
+	JVM_CurrentThread(env, thread_class)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_countStackFrames(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jobject
+) -> jint {
+	JVM_CountStackFrames(env, thread)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_interrupt(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jobject
+) {
+	JVM_Interrupt(env, thread)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_isInterrupted(
+	env: *mut JNIEnv, _this: jobject,
+	thread: jobject, clear_interrupted: jboolean
+) -> jboolean {
+	JVM_IsInterrupted(env, thread, clear_interrupted)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable
+) {
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable
+) {
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable
+) {
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable
+) {
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_dev_binclub_jvm4j_JVM4J_(
+	env: *mut JNIEnv, _this: jobject,
+	throwable: jthrowable
+) {
 }
